239e9ec1678f6c6d234a5c3881ff0cd0d331b004
hongtaigi <xorl21@naver.com>
2023-01-24 02:26:33 +0900

    12

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		
+		String str = "test";
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
