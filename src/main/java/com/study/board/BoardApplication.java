6dad7a6e2132e72d538659a94b928b00646e0afb
hongtaigi <xorl21@naver.com>
2023-01-24 15:38:56 +0900

    12

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -8,8 +8,9 @@ public class BoardApplication {
 
 	public static void main(String[] args) {
 		//String str = "test";
-		String str2 = "test2";
-		String str4 = "test4";
+		//String str2 = "test2";
+		//String str3 = "test3";
+		//String str4 = "test4";
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
