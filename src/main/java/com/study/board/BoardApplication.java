5bbe579f055a575829ac7596cd82eda71af4e941
hongtaigi <90089798+hongtaigi@users.noreply.github.com>
2023-01-24 15:49:30 +0900

    Update BoardApplication.java
    kkkkkkkk

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -7,15 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		String str = "test";
-		String str2 = "test2";
-		String str3 = "test3";
-		String str4 = "test4";
-		String str5 = "test5";
-		//test master
-
-		//test local
-		//test local2
+		//kkkkkkkk
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
