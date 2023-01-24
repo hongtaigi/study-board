0580a30be4f21baecd4fdc415dae2920dba7ec6f
hongtaigi <xorl21@naver.com>
2023-01-24 15:57:55 +0900

    951753852456

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -8,8 +8,10 @@ public class BoardApplication {
 
 	public static void main(String[] args) {
 		//kkkkkkkk
+		//ssssssss
 		//llllllll
 		//99999999
+		//1234
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
