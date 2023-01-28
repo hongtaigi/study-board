c90fe81ff16e8d5cb8c2274be712b9ee388169fc
hongtaigi <xorl21@naver.com>
2023-01-28 20:38:09 +0900

    처움생성

---
+++ src/main/java/com/study/board/board.java
@@ -0,0 +1,13 @@
+package com.study.board;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class board {
+    String abc = "abc";
+    List<Map<String,String>> bList = new ArrayList<Map<String, String>>();
+
+
+}
