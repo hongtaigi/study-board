0ada4c04f79b76061d479052b97aaeefd53078b9
hongtaigi <90089798+hongtaigi@users.noreply.github.com>
2023-01-28 20:43:48 +0900

    Update board.java

--- src/main/java/com/study/board/board.java
+++ src/main/java/com/study/board/board.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public class board {
     String abc = "abc";
     List<Map<String,String>> bList = new ArrayList<Map<String, String>>();
-
+    //서버에서 변경
     //변경--서버에서 변경
+    //서버에서 변경
 }
