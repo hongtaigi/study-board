225ba6812379fa1b606ed3e60251fe1b2010d6e1
hongtaigi <xorl21@naver.com>
2023-08-27 09:07:53 +0900

    test로 upload

--- src/main/java/com/study/board/board.java
+++ src/main/java/com/study/board/board.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 public class board {
     String abc = "abc";
+    String aaa = "aaa";
     List<Map<String,String>> bList = new ArrayList<Map<String, String>>();
     //서버에서 변경
     //변경--서버에서 변경
